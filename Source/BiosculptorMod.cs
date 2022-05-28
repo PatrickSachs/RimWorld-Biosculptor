168b42ac87069a19ea11ac574ab792f143dc4bcf
Patrick Sachs <kontakt@patrick-sachs.de>
2022-05-28 14:51:47 +0200

    Added: Source files

---
+++ Source/BiosculptorMod.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Lilith.RimWorld.Biosculptor {
+    public class BiosculptorMod : Mod {
+        public BiosculptorMod(ModContentPack content) : base(content) {
+            Settings = base.GetSettings<BiosculptorSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect) {
+            Settings.DoSettingsWindowContents(inRect);
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory() {
+            return Settings.SettingsCategory();
+        }
+
+        public static BiosculptorSettings Settings;
+    }
+}
